fixed per-locus distance matrix calculation. Note Fst measures still in development

autostreamtree.py
@@ -1,23 +1,64 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
 
 import autostreamtree.functions as ast
 from autostreamtree.params import parseArgs
 
 def main():
 
-    params = parseArgs()
+	params = parseArgs()
 
-    print("Running autostream tree", params.run, "workflow\n")
-    G = ast.read_network(params.network, params.shapefile)
+	print("Running autostream tree", params.run, "workflow\n")
+	G = ast.read_network(params.network, params.shapefile)
 
 	# read point coordinate data
-    points = pd.read_csv(params.geodb, sep="\t", header="infer")
-    (point_coords, pop_coords, popmap) = ast.processSamples(params, points, G)
+	points = pd.read_csv(params.geodb, sep="\t", header=None)
+	(point_coords, pop_coords, popmap) = ast.processSamples(params, points, G)
 
-    print(popmap)
+	# read genetic data from VCF
+	seqs = ast.read_vcf(params.vcf, concat=params.concat, popmap=popmap)
 
+	# calculate genetic distance matrix
+	if params.run != "STREAMDIST" and params.run != "RUNLOCI":
+		gen=None
+		if not params.genmat:
+			print("\nCalculating genetic distances...")
+			(gen, pop_gen) = ast.getGenDistMats(params, point_coords, popmap, seqs)
+		else:
+			print("\nReading genetic distances from provided matrix:", params.genmat)
+			inmat = pd.read_csv(params.genmat, header=0, index_col=0, sep="\t")
+			(gen, pop_gen) = ast.parseInputGenMat(params, inmat, point_coords, popmap)
+
+		ast.reportPopGenMats(params, gen, pop_gen, point_coords, pop_coords)
+
+		if params.run == "GENDIST":
+			sys.exit(0)
+
+
+	#IMPORTANT NOTE: This function will silently skip any loci for which calculations aren't possible (e.g., population having no data)
+	if params.run == "RUNLOCI":
+		genlist=list()
+		popgenlist=list()
+		for loc in ast.getLocData(seqs):
+			try:
+				(gen, pop_gen) = ast.getGenDistMats(params, point_coords, popmap, loc)
+				genlist.append(gen)
+				popgenlist.append(pop_gen)
+			except Exception:
+				pass
+		print("\nCalculating average of locus-wise genetic distance matrices...")
+		if genlist[0] is not None:
+			gen = np.mean(genlist, axis=0)
+		else:
+			gen = genlist[0]
+		if popgenlist[0] is not None:
+			popgen = np.mean(popgenlist, axis=0)
+		else:
+			popgen = popgenlist[0]
+
+		ast.reportPopGenMatsList(params, genlist, popgenlist, point_coords, pop_coords)
 
 
 #Call main function

autostreamtree/functions.py
@@ -12,6 +12,7 @@ import geopandas as gpd
 import numpy as np
 import networkx as nx
 import seaborn as sns
+from pysam import VariantFile
 from scipy import stats
 from os import listdir
 from os.path import isfile, join
@@ -26,98 +27,12 @@ import pickle
 from math import radians, degrees, sin, cos, asin, acos, sqrt
 
 import autostreamtree.cluster_pops as clust
+import autostreamtree.sequence as seq
+import autostreamtree.genetic_distances as gendist
+import autostreamtree.report_refs as ref
+import autostreamtree.aggregators as agg
+import autostreamtree.Mantel as Mantel
 
-# import riverscape.genetic_distances as gendist
-# import riverscape.cluster_pops as clust
-# import riverscape.report_refs as ref
-# from riverscape.ast_menu import parseArgs
-# import riverscape.aggregators as agg
-# import riverscape.Mantel as Mantel
-
-
-#TODO: Currently assumes all points can be reached by all other points. Add option to check this first, and delete any points that are unreachable
-
-# def main():
-#
-# 	params = parseArgs()
-#
-# 	print("Starting...\n")
-#
-# 	if params.network:
-# 		print("Reading network from saved file: ", params.network)
-# 		G=nx.OrderedGraph(nx.read_gpickle(params.network).to_undirected())
-# 		#print("G:",len(G.edges()))
-# 	else:
-# 		print("Building network from shapefile:",params.shapefile)
-# 		print("WARNING: This can take a while with very large files!")
-# 		G=nx.OrderedGraph(nx.read_shp(params.shapefile, simplify=True, strict=True).to_undirected())
-#
-#
-# 	#parse dataset
-# 	points = pd.read_csv(params.geodb, sep="\t", header="infer")
-# 	(point_coords, pop_coords, popmap, seqs) = processSamples(params, points, G)
-#
-#
-# 	#traverse graph to fill: streamdists, gendists, and incidence matrix
-# 	#calculate genetic distance matrix -- right now that is a JC69-corrected Hamming distance
-# 	#D
-# 	if params.run != "STREAMDIST" and params.run != "RUNLOCI":
-# 		gen=None
-# 		print("\nCalculating genetic distances...")
-# 		if not params.genmat:
-# 			(gen, pop_gen) = getPopGenMats(params, point_coords, popmap, seqs)
-# 		else:
-# 			print("\nReading genetic distances from provided matrix:", params.genmat)
-# 			inmat = pd.read_csv(params.genmat, header=0, index_col=0, sep="\t")
-# 			(gen, pop_gen) = parseInputGenMat(params, inmat, point_coords, popmap)
-#
-# 		reportPopGenMats(params, gen, pop_gen, point_coords, pop_coords)
-#
-# 		if params.run == "GENDIST":
-# 			sys.exit(0)
-#
-# 	#IMPORTANT NOTE: This function will silently skip any loci for which calculations aren't possible (e.g., population having no data)
-# 	if params.run == "RUNLOCI":
-# 		genlist=list()
-# 		popgenlist=list()
-# 		if not params.locmatdir:
-# 			for i in seqs:
-# 				try:
-# 					(gen, pop_gen) = getPopGenMats(params, point_coords, popmap, [i])
-# 					genlist.append(gen)
-# 					popgenlist.append(pop_gen)
-# 				except Exception:
-# 					pass
-# 		else:
-# 			locmats = [f for f in listdir(params.locmatdir) if isfile(join(params.locmatdir, f))]
-# 			locmats = sorted(locmats, key = lambda x: int(x.split(".")[-1]))
-# 			print("\nReading per-locus genetic distances from provided directory:", params.locmatdir)
-#
-# 			for locmat in locmats:
-# 				inmat = pd.read_csv(join(params.locmatdir, locmat), header=0, index_col=0, sep="\t")
-# 				#print(inmat)
-# 				(gen, pop_gen) = parseInputGenMat(params, inmat, point_coords, popmap)
-# 				genlist.append(gen)
-# 				popgenlist.append(pop_gen)
-#
-# 		#get "observed" gen dist matrix, either as a provided matrix
-# 		if not params.genmat:
-# 			print("\nCalculating average of locus-wise genetic distance matrices...")
-# 			if genlist[0] is not None:
-# 				gen = np.mean(genlist, axis=0)
-# 			else:
-# 				gen = genlist[0]
-# 			if popgenlist[0] is not None:
-# 				popgen = np.mean(popgenlist, axis=0)
-# 			else:
-# 				popgen = popgenlist[0]
-# 		else:
-# 			print("\nReading genetic distances from provided matrix:", params.genmat)
-# 			inmat = pd.read_csv(params.genmat, header=0, index_col=0, sep="\t")
-# 			(gen, pop_gen) = parseInputGenMat(params, inmat, point_coords, popmap)
-# 		#print(genlist)
-# 		#print(popgenlist)
-# 		#sys.exit()
 #
 # 	#for ia,ib in itertools.combinations(range(0,len(popmap)),2):
 # 	#	print(popmap.keys()[ia])
@@ -324,6 +239,57 @@ import autostreamtree.cluster_pops as clust
 #
 # 	print("\nDone!\n")
 
+
+
+# read vcf file
+def read_vcf(vcf, concat="none", popmap=None):
+	bcf_in = VariantFile(vcf)
+
+	# set up data dict
+	dat=dict()
+	samples = list((bcf_in.header.samples))
+	for s in samples:
+		if concat == "all":
+			dat[s] = list()
+			dat[s].append(["",""])
+		else:
+			dat[s] = list()
+
+	# if popmap, make list of samples to drop that aren't in a pop
+	if popmap:
+		keep = list()
+		for pop in popmap:
+			keep.extend(popmap[pop])
+		bcf_in.subset_samples(keep)
+
+	chrom="FIRST"
+	for record in bcf_in.fetch():
+		for i, sample in enumerate(record.samples):
+			if concat=="all":
+				print(dat[sample][0])
+				loc = seq.decode(record.samples[i]['GT'], record.ref, record.alts, as_list=True)
+				dat[sample][-1][0]=dat[sample][-1][0]+loc[0]
+				dat[sample][-1][1]=dat[sample][-1][1]+loc[1]
+			elif concat=="loc":
+				if record.chrom != chrom:
+					dat[sample].append(["",""])
+				loc = seq.decode(record.samples[i]['GT'], record.ref, record.alts, as_list=True)
+				dat[sample][-1][0]=dat[sample][-1][0]+loc[0]
+				dat[sample][-1][1]=dat[sample][-1][1]+loc[1]
+			else:
+				loc = seq.decode(record.samples[i]['GT'], record.ref, record.alts)
+				dat[sample].append(loc)
+		chrom=record.chrom
+	if concat != "none":
+		for sample in dat:
+			dat[sample] = ["/".join(x) for x in dat[sample]]
+	for sample in dat.keys():
+		if len(dat[sample]) < 1:
+			del dat[sample]
+		elif len(dat[sample]) == 1 and dat[sample][0][0] == "":
+			del dat[sample]
+	return(dat)
+
 # read network
 def read_network(network, shapefile):
 	if network:
@@ -417,6 +383,35 @@ def reportPopGenMats(params, gen, pop_gen, point_coords, pop_coords):
 		pop_genDF = pd.DataFrame(pop_gen, columns=list(pop_coords.keys()), index=list(pop_coords.keys()))
 		pop_genDF.to_csv((str(params.out) + ".popGenDistMat.txt"), sep="\t", index=True)
 		del pop_genDF
+
+def getLocData(seqs):
+	for loc in range(0, len(seqs[list(seqs.keys())[0]])):
+		d=dict()
+		for ind in seqs.keys():
+			d[ind] = [seqs[ind][loc]]
+		yield(d)
+
+#print and write genmats to file
+def reportPopGenMatsList(params, genlist, popgenlist, point_coords, pop_coords):
+	dir = str(params.out)+"_locmats"
+	os.mkdir(dir)
+	i=0
+	for gen in genlist:
+		if gen is not None:
+			#write individual genetic distances to file
+			ind_genDF = pd.DataFrame(gen, columns=list(point_coords.keys()), index=list(point_coords.keys()))
+			ind_genDF.to_csv((str(dir) + "/loc_" + str(i) + ".indGenDistMat.txt"), sep="\t", index=True)
+			del ind_genDF
+			i+=1
+	j=0
+	for pop_gen in popgenlist:
+		if pop_gen is not None:
+			#write population genetic distances to file
+			#print(list(pop_coords.keys()))
+			pop_genDF = pd.DataFrame(pop_gen, columns=list(pop_coords.keys()), index=list(pop_coords.keys()))
+			pop_genDF.to_csv((str(dir) + "/loc_" + str(j) + ".popGenDistMat.txt"), sep="\t", index=True)
+			del pop_genDF
+			j+=1
 
 # Disable
 def blockPrint():
@@ -623,7 +618,7 @@ def processSamples(params, points, G):
 	return(point_coords, pop_coords, popmap)
 
 #returns population genetic distance matrices
-def getPopGenMats(params, point_coords, popmap, seqs):
+def getGenDistMats(params, point_coords, popmap, seqs):
 	gen = None
 	pop_gen = None
 	if params.dist in ["PDIST", "TN84", "TN93", "K2P", "JC69"]:

autostreamtree/genetic_distances.py
@@ -24,21 +24,16 @@ def getPopGenMat(dist, indmat, popmap, dat, seqs, pop_agg="ARITH", loc_agg="ARIT
 			#print(inds1)
 			genmat[ia,ib] = (agg.aggregateDist(pop_agg, ([indmat[i, j] for i in inds1 for j in inds2])))
 			genmat[ib,ia] = genmat[ia,ib]
-		elif dist == "JOST" or dist == "LINJOST":
+		elif dist == "JOST":
 			results=list()
-			for loc in range(0, len(seqs)):
-				seqs1 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ia]])
-				seqs2 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ib]])
+			for loc in range(0, len(seqs[popmap.values()[ia][-1]])):
+				seqs1 = getAlleles([seqs[x][loc] for x in popmap.values()[ia]])
+				seqs2 = getAlleles([seqs[x][loc] for x in popmap.values()[ib]])
 				if not cleanList(set(seqs1), ["n", "N", "-", "?"]) or not cleanList(set(seqs2), ["n", "N", "-", "?"]):
 					continue
 				results.append(twoPopJostD(seqs1, seqs2, ploidy, global_het))
-			#print(results)
 			if len(results) > 1:
-				if dist=="LINJOST":
-					D = agg.aggregateDist(loc_agg, results)
-					genmat[ia,ib] = genmat[ib,ia] = (1.0/(1.0-float(D)))
-				else:
-					genmat[ia,ib] = genmat[ib,ia] = agg.aggregateDist(loc_agg, results)
+				genmat[ia,ib] = genmat[ib,ia] = agg.aggregateDist(loc_agg, results)
 			elif len(results) < 1:
 				#print("ERROR: population",popmap.values()[ia],"or",popmap.values()[ib],"lacks any data")
 				raise ValueError
@@ -47,9 +42,9 @@ def getPopGenMat(dist, indmat, popmap, dat, seqs, pop_agg="ARITH", loc_agg="ARIT
 		elif dist == "GST" or dist == "GSTPRIME":
 			HT=list()
 			HS=list()
-			for loc in range(0, len(seqs)):
-				seqs1 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ia]])
-				seqs2 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ib]])
+			for loc in range(0, len(seqs[popmap.values()[ia][-1]])):
+				seqs1 = getAlleles([seqs[x][loc] for x in popmap.values()[ia]])
+				seqs2 = getAlleles([seqs[x][loc] for x in popmap.values()[ib]])
 				if not cleanList(set(seqs1), ["n", "N", "-", "?"]) or not cleanList(set(seqs2), ["n", "N", "-", "?"]):
 					continue
 				if dist == "GST" or "GSTPRIME":
@@ -70,9 +65,9 @@ def getPopGenMat(dist, indmat, popmap, dat, seqs, pop_agg="ARITH", loc_agg="ARIT
 		elif dist == "FST" or dist == "LINFST":
 			num = list() #numerator; a
 			denom = list() #denominator; a*b*c
-			for loc in range(0, len(seqs)):
-				seqs1 = cleanInds([seqs[loc][x] for x in popmap.values()[ia]])
-				seqs2 = cleanInds([seqs[loc][x] for x in popmap.values()[ib]])
+			for loc in range(0, len(seqs[popmap.values()[ia][-1]])):
+				seqs1 = cleanInds([seqs[x][loc] for x in popmap.values()[ia]])
+				seqs2 = cleanInds([seqs[x][loc] for x in popmap.values()[ib]])
 				if not all("/" in x for x in seqs1) or not all("/" in x for x in seqs2):
 					print("ERROR: FST estimates require phased data.")
 					sys.exit(1)
@@ -89,13 +84,16 @@ def getPopGenMat(dist, indmat, popmap, dat, seqs, pop_agg="ARITH", loc_agg="ARIT
 			if dist == "FST":
 				genmat[ia,ib] = genmat[ib,ia] = theta
 			elif dist == "LINFST":
-				genmat[ia,ib] = genmat[ib,ia] = (theta / (1-theta))
+				if theta != 1.0:
+					genmat[ia,ib] = genmat[ib,ia] = (theta / (1-theta))
+				else:
+					genmat[ia,ib] = genmat[ib,ia] = theta
 		elif dist == "NEI83":
 			results = list()
 			loci = 0.0
-			for loc in range(0, len(seqs)):
-				seqs1 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ia]])
-				seqs2 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ib]])
+			for loc in range(0, len(seqs[popmap.values()[ia][-1]])):
+				seqs1 = getAlleles([seqs[x][loc] for x in popmap.values()[ia]])
+				seqs2 = getAlleles([seqs[x][loc] for x in popmap.values()[ib]])
 				if not cleanList(set(seqs1), ["n", "N", "-", "?"]) or not cleanList(set(seqs2), ["n", "N", "-", "?"]):
 					#print("WARNING: Skipping locus "+str(loc)+" in comparison of populations "+str(ia)+" and "+str(ib)+": Not enough data.")
 					continue
@@ -105,9 +103,9 @@ def getPopGenMat(dist, indmat, popmap, dat, seqs, pop_agg="ARITH", loc_agg="ARIT
 			genmat[ia,ib] = genmat[ib,ia] = Da
 		elif dist == "EUCLID":
 			results = list()
-			for loc in range(0, len(seqs)):
-				seqs1 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ia]])
-				seqs2 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ib]])
+			for loc in range(0, len(seqs[popmap.values()[ia][-1]])):
+				seqs1 = getAlleles([seqs[x][loc] for x in popmap.values()[ia]])
+				seqs2 = getAlleles([seqs[x][loc] for x in popmap.values()[ib]])
 				if not cleanList(set(seqs1), ["n", "N", "-", "?"]) or not cleanList(set(seqs2), ["n", "N", "-", "?"]):
 					#print("WARNING: Skipping locus "+str(loc)+" in comparison of populations "+str(ia)+" and "+str(ib)+": Not enough data.")
 					continue
@@ -117,9 +115,9 @@ def getPopGenMat(dist, indmat, popmap, dat, seqs, pop_agg="ARITH", loc_agg="ARIT
 		elif dist == "CHORD":
 			num = list()
 			denom = list()
-			for loc in range(0, len(seqs)):
-				seqs1 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ia]])
-				seqs2 = seq.getAlleles([seqs[loc][x] for x in popmap.values()[ib]])
+			for loc in range(0, len(seqs[popmap.values()[ia][-1]])):
+				seqs1 = getAlleles([seqs[x][loc] for x in popmap.values()[ia]])
+				seqs2 = getAlleles([seqs[x][loc] for x in popmap.values()[ib]])
 				if not cleanList(set(seqs1), ["n", "N", "-", "?"]) or not cleanList(set(seqs2), ["n", "N", "-", "?"]):
 					#print("WARNING: Skipping locus "+str(loc)+" in comparison of populations "+str(ia)+" and "+str(ib)+": Not enough data.")
 					continue
@@ -156,9 +154,9 @@ def getGenMat(dist, points, seqs, ploidy, het, loc_agg):
 	#for each combination, calc jukes-cantor corrected distance
 	for ia, ib in itertools.combinations(range(0,len(points)),2):
 		results=list()
-		for loc in range(0, len(seqs)):
-			seq1 = seqs[loc][points.keys()[ia]]
-			seq2 = seqs[loc][points.keys()[ib]]
+		for loc in range(0, len(seqs[points.keys()[ia]])):
+			seq1 = seqs[points.keys()[ia]][loc]
+			seq2 = seqs[points.keys()[ib]][loc]
 			if "/" in seq1:
 				seq1 = seq.DNAconsensus(seq1)
 			if "/" in seq2:
@@ -190,13 +188,11 @@ def jukes_cantor_distance(seq1, seq2, het=False):
 		obs=p_distance(seq1, seq2, trans=False)
 	else:
 		obs=hamming_distance(seq1, seq2, trans=False)
-	#print(obs)
 	#print(1.0 - ((4.0*obs)/3.0))
-	if obs > 0.75:
-		obs = 0.75
-	dist=-0.75*np.log(1.0 - ((4.0*obs)/3.0))
-	#print(dist)
-	if dist <= 0.0:
+	if obs >= 0.75:
+		obs = 0.74999
+	dist=-0.75*np.log(1.0 - ((4.0/3.0)*obs))
+	if not dist > 0.0:
 		return(0.0)
 	return(dist)
 
@@ -490,6 +486,8 @@ def twoPopWeirCockerhamFst(s1, s2):
 		pbar = (ac1+ac2) / (float(len(alleles1)) + float(len(alleles2)))
 		ssquare = ((np.sum( [ (n1* (np.square(p1 - pbar)) ), (n2* (np.square(p2 - pbar))) ])) / ((r-1.0)*nbar))
 		hbar = ((h1+h2) / (r * nbar))
+		# print(nbar)
+		# print(hbar)
 		a = ((nbar/nC) *
 			(ssquare -
 			((1.0 / (nbar-1.0)) *