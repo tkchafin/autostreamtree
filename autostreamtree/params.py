78beb0365dbdabf20ac59b9c16061620f630109e
tkchafin <tylerkchafin@gmail.com>
2023-11-02 17:16:10 +0000

    cleaning up cli menu, made iterative default

--- autostreamtree/params.py
+++ autostreamtree/params.py
@@ -33,12 +33,12 @@ class parseArgs():
         self.clusterpop=False
         self.sclusterpop=False
         self.output_driver="GPKG"
-        self.dist = "JC69"
+        self.dist = "LINFST"
         self.het = False
         self.genmat = None
         self.log = False
         self.and_log = False
-        self.iterative = False
+        self.iterative = True
         self.weight = "CSE67"
         self.permutations = 1000
         self.coercemat = False
@@ -53,7 +53,7 @@ class parseArgs():
         self.pop_agg = "ARITH"
         self.sdist_agg="ARITH"
 
-        self.reachid_col = "REACH_ID"
+        self.reachid_col = "HYRIV_ID"
         self.length_col = "LENGTH_KM"
 
         #dbscan Options
@@ -81,21 +81,21 @@ class parseArgs():
                 if str(arg).lower() in ["none", "all", "loc"]:
                     self.concat = str(arg).lower()
                 else:
-                    self.display_help("Invalid option "+str(arg).lower()+" for option <--concat>")
+                    self.display_help(str("Invalid option "+str(arg).lower()+" for option <--concat>"))
             elif opt == 'v' or opt == 'vcf':
                 self.vcf = arg
             elif opt == 'r' or opt == 'run':
                 self.run = arg.upper()
                 if self.run not in ["ALL", "GENDIST", "IBD", "STREAMDIST", "STREAMTREE", "DISTANCES", "RUNLOCI"]:
-                    self.display_help("Invalid option", arg.upper(),"for option <-r/--run>")
+                    self.display_help(str("Invalid option", arg.upper(),"for option <-r/--run>"))
             elif opt == 'p' or opt == 'pop' or opt == "popmap":
                 self.pop = arg
             elif opt == "g" or opt == "geopop" or opt == "geopops":
                 self.geopop = True
             elif opt == 'd' or opt == 'dist':
                 self.dist = arg.upper()
-                if self.dist not in ["PDIST", "JC69", "K2P", "TN84", "TN93", "FST", "LINFST", "JOST", "NEI83", "EUCLID", "CHORD", "GST", "GSTPRIME", "LINJOST"]:
-                    self.display_help("Invalid option", arg.upper(),"for option <-d/--dist>")
+                if self.dist not in ["PDIST", "JC69", "K2P", "TN84", "TN93", "FST", "LINFST", "JOST", "NEI72", "NEI83", "EUCLID", "CHORD", "GST", "GSTPRIME", "LINJOST"]:
+                    self.display_help(str("Invalid option", arg.upper(),"for option <-d/--dist>"))
             elif opt == "het":
                 self.het = True
             elif opt == "locmatdir":
@@ -110,8 +110,6 @@ class parseArgs():
                 self.log = True
             elif opt == "and_log":
                 self.and_log = True
-            elif opt == "iterative":
-                self.iterative = True
             elif opt == "edge_list":
                 self.edge_list = arg
             elif opt=="clusterpop" or opt=="c":
@@ -129,7 +127,7 @@ class parseArgs():
                 if self.weight=="1" or self.weight=="CSE":
                     self.weight="CSE67"
                 if self.weight not in ["CSE67", "FM67", "BEYER74"]:
-                    self.display_help("Invalid option "+str(arg).upper()+" for option <-w/--weight>")
+                    self.display_help(str("Invalid option "+str(arg).upper()+" for option <-w/--weight>"))
             elif opt == "o" or opt == "out":
                 self.out = arg
             elif opt == "stream_fit":
@@ -157,7 +155,7 @@ class parseArgs():
                 self.ploidy=1
             elif opt=="O" or opt=="gdf_out":
                 if str(arg).upper() not in ["GPKG", "SHP", "GDB"]:
-                    self.display_help("Invalid option "+str(arg).upper()+" for option <--gdf_out>")
+                    self.display_help(str("Invalid option "+str(arg).upper()+" for option <--gdf_out>"))
                 self.output_driver = str(arg).upper()
             elif opt=="global_het":
                 self.global_het=True
@@ -168,11 +166,11 @@ class parseArgs():
             elif opt=="pop_agg" or opt=="P":
                 self.pop_agg = arg.upper()
                 if self.pop_agg not in ["HARM", "ADJHARM", "ARITH", "GEOM", "MEDIAN", "MAX", "MIN"]:
-                    self.display_help("Invalid option "+str(arg).upper()+" for option <--pop_agg>")
+                    self.display_help(str("Invalid option "+str(arg).upper()+" for option <--pop_agg>"))
             elif opt=="loc_agg" or opt=="L":
                 self.loc_agg = arg.upper()
                 if self.loc_agg not in ["HARM", "ADJHARM", "ARITH", "GEOM", "MEDIAN", "MAX", "MIN"]:
-                    self.display_help("Invalid option "+str(arg).upper()+" for option <--loc_agg>")
+                    self.display_help(str("Invalid option "+str(arg).upper()+" for option <--loc_agg>"))
             else:
                 assert False, "Unhandled option %r"%opt
 
@@ -180,18 +178,13 @@ class parseArgs():
             self.display_help("No input provided <-i/--coords>")
         if not self.shapefile and self.run != "GENDIST":
             self.display_help("No shapefile provided <-s/--shp>")
-        # if self.snps and self.dist in ["PDIST", "JC69", "K2P", "TN93"]:
-        #     print("\nWARNING: Can't calculate",str(self.dist),"distances with SNP data. Will instead report proportion of SNPs which are different.")
-        #     print("NOTE: This is equivalent to an uncorrected p-distance (PDIST) calculated on the concatenated SNP data.\n")
-        #     self.snps = False
-        #     self.dist = "PDIST"
         if self.ploidy > 2 or self.ploidy < 1:
-            self.display_help("Ploidy of",self.ploidy,"not currently allowable. Please choose 1 (haploid) or 2 (diploid)")
+            self.display_help(str("Ploidy of",self.ploidy,"not currently allowable. Please choose 1 (haploid) or 2 (diploid)"))
 
         #sanity checks
         if self.dist not in ["PDIST", "TN84", "TN93", "K2P", "JC69"]:
             if not self.pop and not self.geopop:
-                print("ERROR: Distance metric",self.dist,"not possible without --pop or --geopop data.")
+                print(str("ERROR: Distance metric",self.dist,"not possible without --pop or --geopop data."))
                 sys.exit(1)
 
 
@@ -201,95 +194,82 @@ class parseArgs():
             print (message)
         print ("\nautostreamtree.py\n")
         print("Author: Tyler K Chafin, Biomathematics and Statistics Scotland")
-        print ("Description: Computes stream distances and genetic distances for \
-georeferenced DNA sequences, performs tests for isolation-by-distance, \
-and uses a least-squares method to fit distances to stream segments.")
+        print ("Description: Methods for analysing genetic distances in networks.")
         print("""
 Mandatory arguments:
-    -s,--shp    : Path to shapefile containing cleaned, contiguous stream reaches (can also support geodatabase or GPKG files)
-    -i,--input    : Input .tsv file containing sample coordinates
-    -v,--vcf    : Input VCF file containing genotypes
+    -s,--shp        : Path to shapefile containing cleaned, contiguous stream reaches (can also support geodatabase or GPKG files)
+    -i,--input      : Input .tsv file containing sample coordinates
+    -v,--vcf        : Input VCF file containing genotypes
 
 General options:
-    -o,--out    : Output prefix [default="out"]
-    -O,--gbf_out  : Output driver for annotated geodataframe (options "SHP", "GPKG", or "GDB")
-    -C,--concat    : Concatenate all SNPs ("all"), by locus ("loc"), or not at all ("none")
+    -o,--out        : Output prefix [default="out"]
+    -O,--gbf_out    : Output driver for annotated geodataframe (options "SHP", "GPKG", or "GDB")
+    -C,--concat     : Concatenate all SNPs ("all"), by locus ("loc"), or not at all ("none")
     -n,--network    : Provide an already optimized network output from a previous run
-        This will be the $out.network file written by autoStreamTree
-    --overwrite    : Overwrite an input network (Only relevant with --network)
-    -h,--help    : Displays help menu
-    -r,--run    : Run which steps? Options: [all, gendist, ibd, streamdist, streamtree]
-        ALL        : Run all steps
-        GENDIST        : Only calculate genetic distance matrix
-        STREAMDIST    : Only compute pairwise stream distances
-        DISTANCES    : Only compute GENDIST + STREAMDIST
-        IBD        : GENDIST + STREAMDIST + Mantel test
-        STREAMTREE    : GENDIST + STREAMDIST + fit StreamTree model
-        RUNLOCI    : Run STREAMTREE fitting on each locus
+    --overwrite     : Overwrite an input network (Only relevant with --network)
+    -h,--help       : Displays help menu
+    -r,--run        : Run which steps? Options: [all, gendist, ibd, streamdist, streamtree]
+        ALL         : Run all steps
+        GENDIST     : Only calculate genetic distance matrix
+        STREAMDIST  : Only compute pairwise stream distances
+        DISTANCES   : Only compute GENDIST + STREAMDIST
+        IBD         : GENDIST + STREAMDIST + Mantel test
+        STREAMTREE  : GENDIST + STREAMDIST + fit StreamTree model
+        RUNLOCI     : Run STREAMTREE fitting on each locus
     -p,--pop        : Pool individuals based on an input population map tsv file
         NOTE: The location will be taken as the centroid among individual samples
-    -g,--geopop        : Pool individuals having identical coordinates
-    -c,--clusterpop    : Use DBSCAN algorithm to automatically cluster populations
-    --reachid_col    : Attribute name representing primary key in shapefile [default="REACH_ID"]
+    -g,--geopop     : Pool individuals having identical coordinates
+    -c,--clusterpop : Use DBSCAN algorithm to automatically cluster populations
+    --reachid_col   : Attribute name representing primary key in shapefile [default="HYRIV_ID"]
     --length_col    : Attribute name giving length in kilometers [default="LENGTH_KM"]
 
 Genetic distance options:
-    -d,--dist    : Use which metric of distance? Options are:
+    -d,--dist       : Use which metric of distance? Options are:
         Substitution models (individual-based):
-          PDIST            : Uncorrected p-distances [# Differences / Length]
-          JC69             : [default] Jukes-Cantor (1969) corrected p-distances
-          K2P            : Kimura 2-parameter distances
-          *TN84            : Tajima and Nei's (1984) distance
-          *TN93            : Tamura and Nei's (1993) distance
+          PDIST     : Uncorrected p-distances [# Differences / Length]
+          JC69      : [default] Jukes-Cantor (1969) corrected p-distances
         Frequency models (when using --pop):
-          FST            : Weir and Cockerham's Fst formulation (=THETAst)
-          *GST            : Hedrick's (2005) correction of Nei (1987) Gst [=G'st]
-          *GSTPRIME        : Meirmans & Hedrick (2011) corrected G'st [=G''st]
-          LINFST        : [default] Rousset's (1997) Fst [=Fst/(1-Fst)]
-          JOST            : Jost's (2008) D
-          NEI72            : Nei's (1972) standard genetic distance
-          NEI83            : Nei and Chesser (1983) Da
-          EUCLID        : Euclidean distance
-          CHORD            : Cavalli-Sforza and Edwards (1967) chord distance
+          FST       : Weir and Cockerham's Fst formulation (=THETAst)
+          LINFST    : [default] Rousset's (1997) Fst [=Fst/(1-Fst)]
+          JOST      : Jost's (2008) D
+          NEI83     : Nei and Chesser (1983) Da
+          CHORD     : Cavalli-Sforza and Edwards (1967) chord distance
           --NOTE: Individual-based metrics can also be computed for
                     populations. You can set how these are aggregated w/ --pop_agg
-          --NOTE: Multiple loci for PDIST, JC69, K2P, and EUCLID distances
+          --NOTE: Multiple loci for PDIST, and JC69 distances
                   will be reported using the method defined in --loc_agg
-          --NOTE: TN84 will use empirical base frequencies
-    -G,--genmat    : Skip calculation and use the provided labeled .tsv matrix
-    --coercemat    : [Boolean] Coerce negative values in input matrix to zero
-    --het        : [Boolean] Count partial differences [e.g. ind1=T, ind2=W]
+    -G,--genmat     : Skip calculation and use the provided labeled .tsv matrix
+    --coercemat     : [Boolean] Coerce negative values in input matrix to zero
+    --het           : [Boolean] Count partial differences [e.g. ind1=T, ind2=W]
     --global_het    : Estimate Ht using global frequencies (default is averaged over pops)
 
 DBSCAN options (only when --clusterpop):
-    --min_samples    : Minimum samples per cluster [default=1]
-    --epsilon        : Maximum distance (in km) within a cluster [default=20]
+    --min_samples   : Minimum samples per cluster [default=1]
+    --epsilon       : Maximum distance (in km) within a cluster [default=20]
 
 Aggregation options:
     -P,--pop_agg    : Define aggregator function for certain genetic distances in pop samples
     -L,--loc_agg    : Define aggregator function for aggregating locus-wise distances
         All of these can take the following options:
-          ARITH        : [default] Use arithmetic mean
+          ARITH     : [default] Use arithmetic mean
           MEDIAN    : Use median distance
-          HARM        : Use harmonic mean
-          ADJHARM    : Adjusted harmonic mean (see docs)
-          GEOM        : Use geometric mean
-          MIN        : Use minimum distance
-          MAX        : Use maximum distance
+          HARM      : Use harmonic mean
+          ADJHARM   : Adjusted harmonic mean (see docs)
+          GEOM      : Use geometric mean
+          MIN       : Use minimum distance
+          MAX       : Use maximum distance
 
 IBD options:
-    --perm        : Number of permutations for mantel test [def=1000]
-    --and_log    : Also perform IBD steps with log geographic distances
+    --perm          : Number of permutations for mantel test [def=1000]
+    --and_log       : Also perform IBD steps with log geographic distances
 
-StreamTree (see Kaliowski et al. 2008) options:
-    --iterative    : Prevent negative distances using the iterative approach
-    -w,--weight    : Desired weighting for least-squares fitting:
+StreamTree options (see Kalinowski et al. 2008) :
+    -w,--weight : Desired weighting for least-squares fitting:
         Options:
-          FM67            : Fitch and Margoliash (1967) [w = 1/D^2]
-          BEYER74        : Beyer et al. (1974) weights [w = 1/D]
-          CSE67            : [default] Cavalli-Sforza and Edwards (1967) [w = 1]
+          FM67      : Fitch and Margoliash (1967) [w = 1/D^2]
+          BEYER74   : Beyer et al. (1974) weights [w = 1/D]
+          CSE67     : [default] Cavalli-Sforza and Edwards (1967) [w = 1]
 
-*Experimental features (still in development)
 """)
         print()
         sys.exit()
