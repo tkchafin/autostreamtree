a3c88569b79fe3d5fb2560c796ce0f4c37c0f213
tkchafin <tylerkchafin@gmail.com>
2024-02-07 12:24:27 +0000

    html docs

--- generate_docs.sh
+++ generate_docs.sh
@@ -4,10 +4,9 @@
 PACKAGE_NAME="autostreamtree"
 
 # Generate documentation in HTML
-pdoc --html --output-dir ./docs $PACKAGE_NAME
-
-# Assuming pdoc generates a folder with the package name, move it to the root of /docs
-mv ./docs/$PACKAGE_NAME/* ./docs/
+pdoc3 --html --output-dir ./docs ./$PACKAGE_NAME
+mv ./docs/autostreamtree/* ./docs
+rm -rf ./docs/autostreamtree
 
 echo "HTML documentation generated in ./docs/"
 
