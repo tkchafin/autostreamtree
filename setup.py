a9330387c03f88377af5b819392d2974be95b5b9
Tyler Chafin <tylerkchafin@gmail.com>
2024-02-07 11:11:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autostreamtree',
-    version='1.0.1',
+    version='$VERSION',  # Placeholder
     packages=find_packages(),
     entry_points={
         'console_scripts': [
